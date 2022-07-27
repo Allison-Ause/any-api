aa6e19e49a1f1d218b8b7ce2b8a458f71e40c0e1
Allison Ause <allison.ause@gmail.com>
2022-07-27 16:26:59 -0700

    wrote test for seeing if /crystals returns full list

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,20 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+//create test for getting ALL crystal data
+describe('/crystals route', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('returns all crystal data', async () => {
+    const res = await request(app).get('/crystals');
+    expect(res.body.length).toEqual(5);
+    expect.addSnapshotSerializer(
+      res.body[0].toEqual({
+        id: expect.any(String),
+        rare: expect.any(Boolean),
+      })
+    );
   });
   afterAll(() => {
     pool.end();
