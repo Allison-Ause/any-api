8e47c36125ef27099f23891ed14251bf60bb2893
Allison Ause <allison.ause@gmail.com>
2022-07-27 17:41:12 -0700

    added test for confirming receipt of single item by id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,17 +3,26 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-//create test for getting ALL crystal data
 describe('/crystals route', () => {
   beforeEach(() => {
     return setup(pool);
   });
   it('returns all crystal data', async () => {
-    const res = await request(app).get('/crystals');
+    const res = await request(app).get('/');
     expect(res.body.length).toEqual(5);
     expect(res.body[0]).toMatchObject({
       id: expect.any(String),
       rare: expect.any(Boolean),
+    });
+  });
+  it('returns one crystal detail', async () => {
+    const res = await request(app).get('/1');
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'Tigers Eye',
+      meaning: 'Confidence',
+      zodiac_sign: 'Aries',
+      rare: false,
     });
   });
   afterAll(() => {
